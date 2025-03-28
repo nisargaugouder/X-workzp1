14a9f92ce53645d6f41180bb24b2f864c925549b
D U NISARGA <nisargagouder2003@gmail.com>
2025-03-28 08:39:57 +0530

    new change

--- Classes and has-a relations/src/com/xworkz/projector/Port.java
+++ Classes and has-a relations/src/com/xworkz/projector/Port.java
@@ -1,19 +1,19 @@
 package com.xworkz.projector;
 
 public class Port {
-    private String name;
-    private int numberOfPorts;
-    private Port port;
+    private String name="Toca";
+    private final int numberOfPorts;
+    private PortType portType;
 
-    public Port(String name, int numberOfPorts) {
+    public Port(String name, int numberOfPorts,PortType portType) {
         this.name = name;
         this.numberOfPorts = numberOfPorts;
-
+        this.portType=portType;
     }
 
     public void displayPortDetails() {
         System.out.println("Port Name: " + this.name);
         System.out.println("Number of Ports: " + this.numberOfPorts);
-        port.displayPortDetails();
+        portType.displayPortTypeDetails();
     }
 }
--- Classes and has-a relations/src/com/xworkz/projector/Projector.java
+++ Classes and has-a relations/src/com/xworkz/projector/Projector.java
@@ -6,12 +6,16 @@ public class Projector {
     public static void main(String[] args) {
         Lens lens=new Lens(50);
         Lamp lamp=new Lamp("trip",new Warranty("20/1/0","4/2/2025"));
-        Port port=new Port("ton",5);
+        lamp.LampDis();
+        Port port=new Port("ton",5,new PortType("tyo",200));
+        port.displayPortDetails();
         Wire wire=new Wire("brazil",200,new Quality("zoom",5));
+        wire.displayWireDetails();
         Board board=new Board("scate",5,new Capacitor(420));
+        board.BoardDis();
         Button button=new Button("Blue",true);
+        button.displayButtonDetails();
         Remote remote=new Remote("Infosys","Red",new Battery(99,new Voltage(20,"Wat")));
-
-
+        remote.RemoteDis();
     }
 }
