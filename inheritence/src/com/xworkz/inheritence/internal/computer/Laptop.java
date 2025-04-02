5e90c1de2899c942657388360d2e176e8136f015
D U NISARGA <nisargagouder2003@gmail.com>
2025-04-02 18:27:14 +0530

    new changes

--- inheritence/src/com/xworkz/inheritence/internal/computer/Laptop.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.inheritence.internal.computer;
-
-public class Laptop extends Computers {
-    public Laptop() {
-        super();
-        System.out.println("Running non-arg constructor Laptop");
-    }
-}
