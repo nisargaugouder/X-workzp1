6a9a928469fdb541bbb2f85f6ea18b9db2b5a568
D U NISARGA <nisargagouder2003@gmail.com>
2025-04-09 19:45:06 +0530

    toString

---
+++ Assignment1/src/com/xworkz/assignment/internal/car/Car.java
@@ -0,0 +1,22 @@
+package com.xworkz.assignment.internal.car;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
